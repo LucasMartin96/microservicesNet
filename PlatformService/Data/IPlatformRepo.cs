829034676528f8016eb8a28dd934783abde2bfe8
LucasMartin96 <lucasmartin766@gmail.com>
2022-02-19 01:29:03 -0300

    Project and db configuration, platform repo and model added

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAll();
+
+        Platform GetPlatformById(int id);
+
+        void CreatePlatform(Platform platform);
+        
+    }
+}
